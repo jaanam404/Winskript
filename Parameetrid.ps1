c7d00814672839fc8d1a8bda089839a1c8f7e0b5
jaana.malm <jaana.malm@voco.ee>
2022-11-08 03:49:24 -0800

    parameetrid

---
+++ Parameetrid.ps1
@@ -0,0 +1,7 @@
+﻿param(
+$Kasutajanimi,
+$Täisnimi,
+$Kontokirjeldus
+)
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Täisnimi -Description $Kontokirjeldus
