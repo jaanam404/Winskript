5dd54ec9e2bff3534aca25566fd5231d0d704890
jaana.malm <jaana.malm@voco.ee>
2022-11-08 02:05:05 -0800

    kasutajakontod

---
+++ Skript.ps1
@@ -0,0 +1,3 @@
+﻿
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+New-LocalUser "Kasutaja1" -Password $KasutajaParool -FullName "Esmane Tavakasutaja" -Description "Local Cccound - Kasutaja1"
