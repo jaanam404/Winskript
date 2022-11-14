7aa1a648714825f80fb9a4252c8424c6d8cf8082
jaana.malm <jaana.malm@voco.ee>
2022-11-13 23:21:38 -0800

    kasutaja konto loomine

---
+++ kasutaja.ps1
@@ -0,0 +1,5 @@
+﻿echo "Sisesta kasutajanimi kujul eesnimi.perenimi:"
+$kasutaja = Read-Host
+
+$KasutajaParool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+New-LocalUser "$kasutaja" -Password $KasutajaParool -FullName $kasutaja -Description "Lokaalne kasutaja"
