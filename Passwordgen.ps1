71ad30a8b6949f938339148ea44a82902c7bb80b
jaana.malm <jaana.malm@voco.ee>
2022-11-28 06:46:24 -0800

    kasutaja loomine

---
+++ Passwordgen.ps1
@@ -0,0 +1,30 @@
+﻿
+
+
+$KasutajaParool = ConvertTo-SecureString $newPassword -AsPlainText -Force
+$Eesnimi = Read-Host "Sisesta oma eesnimi"
+$Perenimi = Read-Host "Sisesta oma perenimi"
+$Kasutajanimi = $Eesnimi.ToLower() + " " + $Perenimi.ToLower()
+$Taisnimi = $Eesnimi + " " + $Perenimi
+$Kirjeldus = "Lokaalne konto- " + $Kasutajanimi
+Write-Output "Loodav kasutaja on $($Kasutajanimi)"
+$ErrorActionPreference = "SilentlyContinue"
+New-LocalUser $Kasutajanimi -Password $KasutajaParool -FullName $Taisnimi -Description $Kirjeldus
+if(!$?) {
+Write-Output "Tekkinud probleem kasutaja loomisega"
+}
+$ErrorActionPreference = "Stop"
+
+
+Function GenerateStrongPassword ([Parameter(Mandatory=$true)][int]$PasswordLenght)
+{
+    Add-Type -AssemblyName System.Web
+    $PassComplexCheck = $false
+    do {
+        $newPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLenght,1)
+        If ( ($newPassword -cmatch "[A-Z\p{Lu}\s]") `
+        -and ($newPassword -cmatch "[a-z\p{Ll}\s]") `        -and ($newPassword -match "[\d]") `        -and ($newPassword -match "[^\w]")        )        {            $PassComplexCheck=$true        }        } While ($PassComplexCheck -eq $false)    return $newPassword}
+GenerateStrongPassword (8) | Out-File -FilePath .\Desktop\Output.txt
+Get-Content -Path .\Desktop\Output.txt
+
+
---
+++ aduser.ps1
@@ -0,0 +1,33 @@
+﻿$file = "C:\Users\Administrator\Documents\adusers.csv"
+$users = Import-Csv $file -Encoding Default -Delimiter ";"
+foreach ($user in $users) {
+$username = $user.FirstName + "." + $user.Lastname
+$username = $username.Tolower()
+$username = Translit($username)
+$upname = $username + "@sv-kool.local"
+$displayname = $user.FirstName + " " + $user.LastName
+echo $displayname
+New-ADUser -Name $username `
+-DisplayName $displayname `-GivenName $user.FirstName `-Surname $user.LastName `-Department $user.Department `-Title $user.Role `-UserPrincipalName $upname `-AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -Force) -Enabled $true
+}
+function Translit 
+{
+param(
+[string] $inputString
+)}
+$Translit = @{
+[char]'ä' = "a"
+[char]'ö' = "o"
+[char]'ü' = "u"
+[char]'õ' = "o"
+}
+$outputString=""
+foreach ($character in $inputCharacter = $inputString.ToCharArray())
+{
+if ($Translit[$character] -cne $null ){
+$outputString += $Translit[$character]
+} else {
+$outputString += $character
+}
+}
+Write-Output $outputString
